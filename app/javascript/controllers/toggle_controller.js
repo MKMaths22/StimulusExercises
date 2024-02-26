254dac4b849ed2f777675d041a7759b9dc400d3c
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-02-26 11:49:08 +0000

    Add dropdown menu to index view. Write toggle#reveal method, using toggle#connect to make menu hidden initially, instead of a class in the HTML.

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,11 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = [ "div" ]
+  static targets = [ "div", "menu" ]
+
+  connect() {
+    this.menuTargets.forEach((menu) => {
+      menu.hidden = true
+    })
+  }
 
   hideorreveal() {
     this.divTargets.forEach((element) => {
       element.hidden = !element.hidden
+    })
+  }
+
+  reveal() {
+    this.menuTargets.forEach((element) => {
+      element.hidden = false
     })
   }
 }
