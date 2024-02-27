5a40758703a9b0066b2acc79a90a3520d9a5b442
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-02-27 11:46:37 +0000

    Change multi-word variable names to use underscores for the text_controller and in the index too. Remove console debugging command for this.counter.

--- app/javascript/controllers/text_controller.js
+++ app/javascript/controllers/text_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static values = { counter: Number, limit: Number, margin: Number }
-  static targets = [ "limitedtext", "errordiv", "warning", "error"]
+  static targets = [ "limited_text", "error_div", "warning", "error"]
   
   connect() {
     this.limit = 280
@@ -11,28 +11,27 @@ export default class extends Controller {
     this.warningTarget.hidden = true
   }
   
-  checklength() {
-    this.counter = this.limitedtextTarget.value.length
-    console.log(this.counter)
+  check_length() {
+    this.counter = this.limited_textTarget.value.length
     
     if (this.counter <= (this.limit - this.margin))
       {this.warningTarget.hidden = true
       this.errorTarget.hidden = true
-      this.errordivTarget.classList.remove("warning")
-      this.errordivTarget.classList.remove("error")
+      this.error_divTarget.classList.remove("warning")
+      this.error_divTarget.classList.remove("error")
       return }
     else if (this.counter > this.limit)
       {
       this.warningTarget.hidden = true
       this.errorTarget.hidden = false
-      this.errordivTarget.classList.remove("warning")
-      this.errordivTarget.classList.add("error")
+      this.error_divTarget.classList.remove("warning")
+      this.error_divTarget.classList.add("error")
       return }
     else
       this.warningTarget.hidden = false
       this.errorTarget.hidden = true
-      this.errordivTarget.classList.add("warning")
-      this.errordivTarget.classList.remove("error")
+      this.error_divTarget.classList.add("warning")
+      this.error_divTarget.classList.remove("error")
   }
 
   
